b951d228435a27a0ec24cccd9ec861e7b352cd4c
furukawatakao <furukawatakao0926@gmail.com>
2019-08-29 16:24:43 +0900

    2019/08/29 二回め

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,4 @@
 class Cart < ApplicationRecord
   has_many :cart_items
-  belongs_to :user_id
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_134545) do
+ActiveRecord::Schema.define(version: 2019_08_29_072117) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_134545) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "products", force: :cascade do |t|
