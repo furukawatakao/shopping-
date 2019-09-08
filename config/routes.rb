ad182a306c217ac55da2abc972de864259957d62
hirudai <hirudai@gmail.com>
2019-09-08 22:15:40 +0900

    purchaseアクションを定義

--- config/routes.rb
+++ config/routes.rb
@@ -11,21 +11,22 @@ Rails.application.routes.draw do
     resources :management_users
   end
 
-    get '/login', to: 'sessions#new'
-    post 'login', to: 'sessions#create'
-    delete '/logout', to: 'sessions#destroy'
-    resources :products, :only => [:index, :show] do
-      collection do
-        post '/search', to: 'products#search'
-      end
+  get '/login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :products, :only => [:index, :show] do
+    collection do
+      post '/search', to: 'products#search'
     end
-    resources :users, :except => :index
+  end
+  resources :users, :except => :index
 
-      resources :carts, only: [:show] do
+  resources :carts, only: [:show] do
     member do
       post '/add_item/:product_id' => 'carts#add_item', as: 'add_item'
       post '/update_item/:product_id' => 'carts#update_item', as: 'update_item'
       delete '/delete_item/:product_id' => 'carts#delete_item', as: 'delete_item'
+      post '/purchase/:cart_id' => 'carts#purchase', as: 'purchase'
     end
   end
 end
