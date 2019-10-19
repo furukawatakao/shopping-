90a33f58162ac29e16cf2f138d46d48f35fa16ec
furukawatakao <furukawatakao0926@gmail.com>
2019-10-19 18:18:58 +0900

    added s3 to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
+  config.active_storage.service = :amazon
+  config.assets.initialize_on_precompile = false
 
-config.active_storage.service = :amazon
+end
