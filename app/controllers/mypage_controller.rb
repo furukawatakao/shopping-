128fd5669d30626ce3189a54209d4901e36a0174
furukawatakao <furukawatakao0926@gmail.com>
2019-10-30 08:32:20 +0900

    10月30日

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -2,7 +2,7 @@ class MypageController < ApplicationController
   before_action :login_required
 
   def show
-    @purchase_historys = OrderItem.eager_load(:product).where(user_id: current_user.id)
+    @purchase_historys = OrderItem.eager_load(:product).where(user_id: current_user.id).order(order_id: :desc )
     @order = Order.eager_load(:order_items => :product).where(user_id: current_user.id).order(created_at: :desc)
   end
 
